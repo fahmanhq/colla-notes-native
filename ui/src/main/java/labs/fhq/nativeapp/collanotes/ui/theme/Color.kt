e9640b9986ee69f19d304fceff28979410bee4ba
Fahman Haqqi <fahmanhq@gmail.com>
2024-08-14 10:35:38 +0700

    init projects

---
+++ ui/src/main/java/labs/fhq/nativeapp/collanotes/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package labs.fhq.nativeapp.collanotes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val P_Purple = Color(0xFF743FF9)
+val P_Cyan = Color(0xFF5FE7A3)
+val P_Yellow = Color(0xFFEDB900)
+val P_Red = Color(0xFFF53950)
+val P_Blue = Color(0xFFF53950)
+val P_Black = Color(0xFF000000)
+val P_White = Color(0xFFFFFFFF)
+val P_Grey = Color(0xFFEFEFEF)
